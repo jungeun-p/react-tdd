0f96f3f84b685c611d4afab76195422cd8306230
jungeun-p <mrk7753@naver.com>
2022-07-25 20:22:25 +0900

    [ modify ] Products Image TDD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Calculator from "./Calc/Calculator";
+import OrderPage from "./pages/OrderPage/OrderPage";
 import SummaryPage from "./pages/SummaryPage/SummaryPage";
 
 function App() {
 
   return (
     <>
+      <OrderPage />
       <SummaryPage />
     </>
   );
---
+++ src/pages/OrderPage/OrderPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Type from './Type';
+
+const OrderPage = () => {
+    return (
+        <>
+            <h1>OrderPage</h1>
+            <Type />
+        </>
+    );
+};
+
+export default OrderPage;

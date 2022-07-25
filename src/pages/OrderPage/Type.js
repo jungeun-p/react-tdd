38c2837608f9357fcfccd0ce111a86ffe11fb168
jungeun-p <mrk7753@naver.com>
2022-07-25 20:58:28 +0900

    [ modify ] OrderImage error TDD

--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import ErrorBanner from '../../components/ErrorBanner';
 import Products from './Products';
 
 const Type = ({ orderType }) => {
     const [items, setItems] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         loadItems(orderType);
-    }, [])
+    }, [orderType]);
+
     const loadItems = async (orderType) => {
         try{
             const res = await axios.get(`http://localhost:5000/${orderType}`);
             setItems(res.data);
         } catch(e){
-            console.log(e);
+            setError(true);
         }
+    }
+
+    if(error){
+        return <ErrorBanner message="에러가 발생했습니다."/>
     }
 
     const ItemComponents = orderType === "products" ? Products : null;

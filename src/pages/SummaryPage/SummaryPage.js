c8cd4f8694bb10afb8ccd07a1e6cbdc3a2bcd220
jungeun-p <mrk7753@naver.com>
2022-07-27 17:40:04 +0900

    [ modify ] Summary Page for Products and Options Array

--- src/pages/SummaryPage/SummaryPage.js
+++ src/pages/SummaryPage/SummaryPage.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { OrderContext } from '../../contexts/OrderContext';
 
-const SummaryPage = () => {
+const SummaryPage = ({ setStep }) => {
     const [checked, setChecked] = useState(false);
+    const [orderDatas] = useContext(OrderContext);
+
+    const productsArray = Array.from(orderDatas.products);
+    const productList = productsArray.map(([key, value]) => (
+        <li key={key}>{value} {key}</li>
+    ));
+    
+    let optionsRender = null;
+    const hasOptions = orderDatas.options.size > 0; 
+    if(hasOptions){
+        const optionsArray = Array.from(orderDatas.options.keys());
+        const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+        // jsx
+        optionsRender = (
+            <>
+                <h2>옵션: {orderDatas.totals.options}</h2>
+                <ul>{optionList}</ul>
+            </>
+        )
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setStep(2);
+    }
+
     return (
         <div>
-            <form>
+            <h1>주문 확인</h1>
+            <h2>여행 상품: {orderDatas.totals.products}</h2>
+            <ul>
+                {productList}
+            </ul>
+            {optionsRender}
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="checkbox"
                     checked={checked}
@@ -13,10 +46,7 @@ const SummaryPage = () => {
                 />
                 <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
                 <br />
-                <button 
-                    disabled={!checked} 
-                    type="submit"
-                > 
+                <button disabled={!checked} type="submit"> 
                     주문 확인
                 </button>
             </form>
